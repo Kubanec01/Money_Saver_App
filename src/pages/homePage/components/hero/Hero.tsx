1c37d5cacd651df3e0887665c885994c64d89969
Jakub Roman <jakub.m.roman@gmail.com>
2025-01-13 22:16:39 +0100

    .

--- src/pages/homePage/components/hero/Hero.tsx
+++ src/pages/homePage/components/hero/Hero.tsx
@@ -1,27 +1,29 @@
-import img1 from "../../../../assets/saturn-img.png";
+import style from "./hero.module.scss";
 
 export const Hero = () => {
   return (
-    <div className="border mx-auto w-[80%] max-w-[1300px] flex justify-center items-center h-[400px] mt-[200px]">
+    <div className="mx-auto w-[90%] max-w-[1100px] flex justify-center items-center mt-[320px]">
       {/* LEFT */}
-      <div className="border h-full w-[50%]">
-        <h1 className="text-white">
-          Good Evening <span>Jacob</span> <br />
-          <span>Let's Save Your Money</span> 🚀
+      <div className="h-full w-[50%] font-medium">
+        <h1 className={`${style.title} text-white text-6xl tracking-tight`}>
+          Good Evening <span>Jacob</span>
         </h1>
-        <button className="text-white">Mission</button>
+        <h2 className="mt-3 mb-1 text-white text-6xl tracking-tight">
+          Today's Mission: <br />
+        </h2>
+          <span className="text-5xl text-[#ffffff] tracking-normal">
+            Save Your Money🪐
+          </span>
+        <p
+        className={`${style.title2} text-[#ffffffad] text-xl font-thin mt-6`}>
+          This app is here to help you improve your saving habits, boost your
+          financial literacy, and provide you with essential everyday info. Join
+          us on a journey where we'll take your finances to the next level and
+          save money on an intergalactic scale!
+        </p>
       </div>
       {/* RIGHT */}
-      <div className="border flex items-center justify-center h-full w-[50%]">
-        <img
-          style={{
-            rotate: "-30deg",
-          }}
-          className="object-cover w-full h-full"
-          src={img1}
-          alt="saturn-img"
-        />
-      </div>
+      <div className="h-full w-[50%]"></div>
     </div>
   );
 };
