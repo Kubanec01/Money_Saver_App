60cc1e78797d433ce4d02adbb84165c4c6521370
Jakub Roman <jakub.m.roman@gmail.com>
2025-03-10 18:58:47 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import "./utils/i18n.ts";
 import { BrowserRouter } from "react-router";
 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="Money_Saver_App">
