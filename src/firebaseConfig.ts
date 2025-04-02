b9a990e048601ea16546b109cf00bd92ecb6c6e6
Jakub Roman <jakub.m.roman@gmail.com>
2025-04-02 21:39:01 +0200

    .

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnoWR5VrC-ZTnDX4yuTmT9PtCFj7vMHO0",
+  authDomain: "money-saver-app-f9d5e.firebaseapp.com",
+  projectId: "money-saver-app-f9d5e",
+  storageBucket: "money-saver-app-f9d5e.firebasestorage.app",
+  messagingSenderId: "863400519457",
+  appId: "1:863400519457:web:89a28e5e84c31afdaac060"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
