c825d93e5a3d6fa9f1d4c213178e67c2fb90c265
Jakub Roman <jakub.m.roman@gmail.com>
2025-05-24 16:20:44 +0200

    404 git fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.tsx";
 import "./utils/i18n.ts";
-import { BrowserRouter } from "react-router";
-
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/Money_Saver_App/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
